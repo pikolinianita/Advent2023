fad2ee56ae0754b4d2d818aecbdc7bf0e7fd2d01
pikolinianita <lukaszsobczak@yahoo.com>
2023-12-01 16:55:51 +0100

    Refactor Day1

--- src/test/java/pl/lcc/Day1Test.java
+++ src/test/java/pl/lcc/Day1Test.java
@@ -1,14 +1,12 @@
 package pl.lcc;
 
-import static org.assertj.core.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class Day1Test {
     @ParameterizedTest
@@ -21,14 +19,14 @@ class Day1Test {
             "42xccx35, 45"}
     )
     void processLineP1Test(String line, String answer) {
-        assertThat(new Day1().decipherP1(line)).isEqualTo(Integer.parseInt(answer));
+        assertThat(new Day1().decipherLinePart1(line)).isEqualTo(Integer.parseInt(answer));
     }
 
     @Test
-    void answerP1Test(){
-       var input = Stream.of("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet");
-       int answer = new Day1().solveP1(input);
-       assertThat(answer).isEqualTo(142);
+    void answerP1Test() {
+        var input = Stream.of("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet");
+        int answer = new Day1().solveTask1(input);
+        assertThat(answer).isEqualTo(142);
     }
 
 }
