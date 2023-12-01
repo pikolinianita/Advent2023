94646c6945d3350559283723a4667494e630ba5a
pikolinianita <lukaszsobczak@yahoo.com>
2023-12-01 13:02:44 +0100

    add sum all lines

--- src/main/java/pl/lcc/Day1.java
+++ src/main/java/pl/lcc/Day1.java
@@ -1,9 +1,19 @@
 package pl.lcc;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Day1 {
 
-
     public int decipherP1(String line) {
-        throw new RuntimeException("not Supported yet. param:" + line);
+        String[] splittedLine = line.split("[a-z]+", 0);
+        var firstNumber = splittedLine[0].isEmpty()? splittedLine[1] : splittedLine[0];
+        return Integer.parseInt(firstNumber + splittedLine[splittedLine.length-1]);
+    }
+
+    public int solveP1(Stream<String> stream) {
+       return stream
+                .mapToInt(this::decipherP1)
+                .sum();
     }
 }
--- src/test/java/pl/lcc/Day1Test.java
+++ src/test/java/pl/lcc/Day1Test.java
@@ -1,8 +1,12 @@
 package pl.lcc;
 
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +18,14 @@ class Day1Test {
             "a1b2c3d4e5f,15",
             "treb7uchet,77"}
     )
-    void testProcessLineP1(String line, String answer) {
-        System.out.println(line + " : " + answer);
+    void processLineP1Test(String line, String answer) {
         assertThat(new Day1().decipherP1(line)).isEqualTo(Integer.parseInt(answer));
+    }
+
+    @Test
+    void answerP1Test(){
+       var input = Stream.of("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet");
+       int answer = new Day1().solveP1(input);
+       assertThat(answer).isEqualTo(142);
     }
 }
