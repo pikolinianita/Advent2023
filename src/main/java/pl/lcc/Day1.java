81d09316203ae877ce5f60a75c3d3813a1b17951
pikolinianita <lukaszsobczak@yahoo.com>
2023-12-01 13:22:03 +0100

    add file read and main

--- src/main/java/pl/lcc/Day1.java
+++ src/main/java/pl/lcc/Day1.java
@@ -1,6 +1,9 @@
 package pl.lcc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Day1 {
@@ -15,5 +18,15 @@ public class Day1 {
        return stream
                 .mapToInt(this::decipherP1)
                 .sum();
+    }
+
+    public static void main(String[] args) {
+        try(var sc =new Scanner(new File("data\\day01.txt"))){
+            Day1 day1 = new Day1();
+            var result = day1.solveP1(sc.tokens());
+            System.out.println(result);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Scanner Failed No File");
+        }
     }
 }
